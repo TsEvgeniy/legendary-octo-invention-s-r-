eb5b094801305d35b15905ed49ca31b356e6cc1f
TsEvgeniy <evgeniy.tsvetikov@icloud.com>
2022-03-09 17:33:45 +0500

    First Initial

---
+++ src/components/ResultsList.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import { withNavigation } from 'react-navigation';
+import ResultsDetail from './ResultsDetail';
+
+const ResultsList = ({ results, title, navigation }) => {
+  if (!results.length) {
+    return null;
+  }
+
+  return (
+    <View style={styles.containerStyle}>
+      <Text style={styles.titleStyle}>{title}</Text>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate('ResultsShow', { id: item.id })
+              }
+            >
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    marginLeft: 15,
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  containerStyle: {
+    marginBottom: 10,
+  },
+});
+
+export default withNavigation(ResultsList);
